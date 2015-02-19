cbab9fc3b4f2d83e072aa20159b34feb370b338e
Pablo Torres <pablo.torres@deri.org>
2015-02-19 12:30:41 +0000

    python stup tool added

---
+++ README.md
@@ -0,0 +1,17 @@
+HDT Connector
+=============
+
+This is a python module to process Dbpedia HDT files.
+
+Installation
+------------
+
+* You must first compile and install HDT and Boost libraries.
+* Once compiled, please provide the respective paths (HDT, Boost) to setup.py
+* It is recomendable to use virtualenv to manage the paths when executing. For example you can set LD\_LIBRARY\_PATH in the activation script (MYENV/bin/activate).
+* You can install the module normally: ``python setup.py install``
+
+Usage
+-----
+
+
---
+++ setup.py
@@ -0,0 +1,95 @@
+import os
+from setuptools import setup
+from setuptools.command.install import install
+from distutils.command.build import build
+from subprocess import call
+from multiprocessing import cpu_count
+
+hdt_include_path = "/home/pablo/workspace/c++/hdt-cpp/hdt-lib/include"
+hdt_libs_path = "/home/pablo/workspace/c++/hdt-cpp/hdt-lib"
+boost_include_path = "/opt/usr/local/boost-1.56.0/include"
+boost_libs_path = "/opt/usr/local/boost-1.56.0/lib"
+
+BASEPATH = os.path.dirname(os.path.abspath(__file__))
+HDTCONNECTOR_PATH = os.path.join(BASEPATH, 'hdt-connector')
+
+class build_hdtconnector(build):
+  def run(self):
+    # Run original build code
+    build.run(self)
+
+    # Configure HDTConnector
+    build_path = os.path.abspath(self.build_temp)
+    cmd_configure = [
+      './configure',
+      'HDT_CFLAGS=-I' + hdt_include_path,
+      'HDT_LIBS=-L' + hdt_libs_path,
+      'boost_CFLAGS=-I' + boost_include_path,
+      'boost_LIBS=-L' + boost_libs_path,
+      '--prefix=' + build_path
+    ]
+
+    cmd_make = [
+      '/bin/bash',
+      '-c',
+      'make && make install'
+    ]
+    target_files = [os.path.join(build_path, 'bin/hdtconnector.so')]
+
+    def configure():
+      print('*'*80)
+      call(cmd_configure, cwd=HDTCONNECTOR_PATH)
+      print('*'*80)
+
+    def compile():
+      print('*'*80)
+      call(cmd_make, cwd=HDTCONNECTOR_PATH)
+      print('*'*80)
+
+    self.execute(configure, [], 'configuring hdtconnector')
+    self.execute(compile, [], 'compiling htdconnector')
+    
+    # Copy hdt to library build folder
+    self.copy_tree(build_path + "/bin", self.build_lib)
+
+class install_hdtconnector(install):
+  def initialize_options(self):
+    install.initialize_options(self)
+    self.build_scripts = None
+
+  def finalize_options(self):
+    install.finalize_options(self)
+    self.set_undefined_options('build', ('build_scripts', 'build_scripts'))
+
+  def run(self):
+    # Run original install
+    install.run(self)
+
+    # Install HDT connector
+    print("Installing hdtconnector")
+    self.copy_tree(self.build_lib, self.install_lib)
+
+def readme():
+  with open('README.md') as f:
+    return f.read()
+
+setup(
+  name = 'hdtconnector',
+  version = '1.0',
+  description = 'HDT connector for DBpedia',
+  long_description = readme(),
+  classifiers = [
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: C++',
+  ],
+  url = 'http://github.com/ptorrestr/hdt-connector',
+  author = 'Pablo Torres',
+  author_email = 'pablo.torres.t@gmail.com',
+  license = 'GNU',
+  zip_safe = False,
+  cmdclass = {
+    'build' : build_hdtconnector,
+    'install' : install_hdtconnector,
+  }
+)
+    
