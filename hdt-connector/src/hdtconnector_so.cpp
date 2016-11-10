d1645a510df4c015d5933c3a7fc59cdca82b556d
Pablo Torres <pablo.torres@insight-centre.org>
2016-11-10 09:29:26 +0000

    update connector name

--- hdt-connector/src/hdtconnector_so.cpp
+++ hdt-connector/src/hdtconnector_so.cpp
@@ -9,7 +9,7 @@ static boost::shared_ptr<HDTConnector> makeClass(const string &val)
 	return boost::shared_ptr<HDTConnector>(new HDTConnector(val));
 }
 
-BOOST_PYTHON_MODULE(hdtconnector)
+BOOST_PYTHON_MODULE(libhdtconnector)
 {
 	class_<HDTIterator, boost::shared_ptr<HDTIterator> >("HDTIterator", init<IteratorTripleString *>())
 		.def("has_next", &HDTIterator::has_next)
