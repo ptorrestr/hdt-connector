095d014eba0e084f48c074332d2b91288f4a50bd
Pablo Torres <pablo.torres@insight-centre.org>
2017-01-24 12:39:34 +0000

    show logs

--- setup.py
+++ setup.py
@@ -88,7 +88,7 @@ class build_hdtconnector(build):
 
     self.execute(autogen, [], 'generating autotools')
     self.execute(configure, [], 'configuring hdtconnector')
-    self.execute("cat libhdtconnector/config.log")
+    self.execute("cat libhdtconnector/config.log", [], 'cat')
     self.execute(compile, [], 'compiling htdconnector')
     if platform.system() == "Darwin":
       self.execute(symlink, [], 'adding symlink for htdconnector')
