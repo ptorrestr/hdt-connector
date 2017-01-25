d6301d1980f8ba4899b1b8ead22d751229360ea8
Pablo Torres <pablo.torres@insight-centre.org>
2017-01-25 10:30:05 +0000

    use system compiler instead of porting the compiler in conda

--- .conda/build.sh
+++ .conda/build.sh
@@ -7,17 +7,18 @@ LIBRARY_PATH="-L${PREFIX}/lib"
 
 if [ "$(uname)" == "Darwin" ]; then
   ln -s ${PREFIX}/lib ${PREFIX}/lib64
-	MACOSX_VERSION_MIN=10.9
+	MACOSX_VERSION_MIN=10.11
   CXXFLAGS="-mmacosx-version-min=${MACOSX_VERSION_MIN}"
-  CXXFLAGS="${CXXFLAGS} -stdlib=libc++ -std=c++11"
+  CXXFLAGS="${CXXFLAGS} -stdlib=libc++"
   LINKFLAGS="-mmacosx-version-min=${MACOSX_VERSION_MIN}"
   LINKFLAGS="${LINKFLAGS} -stdlib=libc++ -L${LIBRARY_PATH}"
 
   HDT_CFLAGS=${PREFIX}/include \
     HDT_LIBS=${PREFIX}/lib \
     BOOST_ROOT=${PREFIX} \
-    PKG_CONFIG_PATH="${PKG_CONFIG_PATH}" \
-    python setup.py install
+		CXX=llvm-g++ \
+		CC=llvm-gcc \
+		python setup.py install
 	unlink ${PREFIX}/lib64
 fi
 
@@ -27,8 +28,8 @@ if [ "$(uname)" == "Linux" ]; then
     HDT_LIBS=${PREFIX}/lib \
     BOOST_ROOT=${PREFIX} \
     PKG_CONFIG_PATH="${PKG_CONFIG_PATH}" \
-		CXX=${PREFIX}/bin/g++ \
-	  CC=${PREFIX}/bin/gcc \
+		CXX=g++ \
+	  CC=gcc \
     python setup.py install
   unlink ${PREFIX}/lib64
 fi
