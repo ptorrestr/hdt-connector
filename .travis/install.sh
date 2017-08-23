7d1c22a4095db061888f531ee7e70ac9f633a2f5
Pablo Torres <pablo.torres@insight-centre.org>
2017-08-23 11:30:44 +0100

    remove rudant code in install. Make more clear the environments

--- .travis/install.sh
+++ .travis/install.sh
@@ -22,12 +22,8 @@ conda config --set always_yes yes --set changeps1 no
 conda update -q conda
 conda info -a
 
-conda config --add channels salford_systems
 conda config --add channels conda-forge
 conda config --add channels ptorrestr
-conda config --append channels pkgw
 conda config --get channels
-conda create -q -n test-environment python=$TRAVIS_PYTHON_VERSION
 conda install conda-build anaconda-client
-source activate test-environment
 conda build .conda/ --no-test --no-anaconda-upload
