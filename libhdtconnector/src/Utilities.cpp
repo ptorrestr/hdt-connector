0d97477f294458216198a86802ede4b3e9e81fe3
Pablo Torres <pablo.torres@insight-centre.org>
2017-10-18 16:19:10 +0100

    use boost_locale when glibc version is too old and codecvt is not available

--- libhdtconnector/src/Utilities.cpp
+++ libhdtconnector/src/Utilities.cpp
@@ -1,15 +1,29 @@
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
 #include "Utilities.h"
 
+#ifdef HAVE_GLIB_2_2X
 wstring_convert<codecvt_utf8_utf16<wchar_t>> Utilities::converter;
+#endif
 
 string
 Utilities::unicode_to_utf8(const wstring& unicode)
 {
+#ifdef HAVE_GLIB_2_2X
   return converter.to_bytes(unicode);
+#elif HAVE_BOOST_LOCALE
+  return utf_to_utf<char>(unicode.c_str(), unicode.c_str() + unicode.size());
+#endif
 }
 
 wstring
 Utilities::utf8_to_unicode(const string& utf8)
 {
+#ifdef HAVE_GLIB_2_2X
   return converter.from_bytes(utf8);
+#elif HAVE_BOOST_LOCALE
+  return utf_to_utf<wchar>(unicode.c_str(), unicode.s_str() + unicode.size());
+#endif
 }
--- libhdtconnector/src/include/Utilities.h
+++ libhdtconnector/src/include/Utilities.h
@@ -1,16 +1,29 @@
 #ifndef UTILITIES_H
 #define UTILITIES_H
 
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#ifdef HAVE_GLIB_2_2X
 #include <locale>
 #include <codecvt>
+#elif HAVE_BOOST_LOCALE
+#include <boost/locale/encoding_utf.hpp>
+#endif
 #include <string>
 
 using namespace std;
+#ifdef HAVE_BOOST_LOCALE
+using boost::locale::conv::utf_to_utf;
+#endif
 
 class Utilities
 {
 private:
+#ifdef HAVE_GLIB_2_2X
   static wstring_convert<codecvt_utf8_utf16<wchar_t>> converter;
+#endif
 
 public:
   static string unicode_to_utf8(const wstring& unicode); 
